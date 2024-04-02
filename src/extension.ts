caa95897fb629cef6296a44a37bf3bff95df9e5b
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-04-02 20:37:40 -0300

    style: extension main files

--- src/extension.ts
+++ src/extension.ts
@@ -12,14 +12,15 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('regex-match.helloWorld', () => {
+  const disposable = vscode.commands.registerCommand('regex-match.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Regex Match!');
+    return vscode.window.showInformationMessage('Hello World from Regex Match!');
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // This method is called when your extension is deactivated
+}
