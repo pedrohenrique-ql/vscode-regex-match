e52e76d8329761a84481be8c934f7c0e8e8afb77
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-04-28 11:36:40 -0300

    test: regex tester returning the document match range

--- src/tests/unit/regex-tester.test.ts
+++ src/tests/unit/regex-tester.test.ts
@@ -11,74 +11,88 @@ vi.mock('vscode', () => ({
 
 describe('Regex Tester', () => {
   it('should test regex correctly, if the regex matches and has one match', () => {
-    const regex = new RegExp('[0-9]a', 'gd');
+    const regex = new RegExp('[0-9]a', 'g');
 
     const parsedRegexTest: ParsedRegexTest = {
       matchingRegex: regex,
       testLines: ['bbb9abbbb'],
+      startTestIndex: 0,
     };
 
     const matchResult = RegexTester.testRegex(parsedRegexTest);
-
     expect(matchResult.length).toBe(1);
-    expect(matchResult[0].matched).toBe(true);
-    expect(matchResult[0].indexes).toEqual([[3, 5]]);
-    expect(matchResult[0].line).toBe(parsedRegexTest.testLines[0]);
+
+    expect(matchResult[0].substring).toBe('9a');
+    expect(matchResult[0].range).toEqual([3, 5]);
   });
 
-  it('should test regex correctly, if the regex matches and has multiple matches', () => {
-    const regex = new RegExp('[0-9]a', 'gd');
+  it('should test regex correctly, if the regex matches and has multiple matches in one line', () => {
+    const regex = new RegExp('[0-9]a', 'g');
 
     const parsedRegexTest: ParsedRegexTest = {
       matchingRegex: regex,
-      testLines: ['9abbbbbbbbb9abbbbb9a'],
+      testLines: ['9abbbbbbbbb8abbbbb7a'],
+      startTestIndex: 0,
     };
 
     const matchResult = RegexTester.testRegex(parsedRegexTest);
+    expect(matchResult.length).toBe(3);
 
-    expect(matchResult.length).toBe(1);
-    expect(matchResult[0].matched).toBe(true);
-    expect(matchResult[0].indexes).toEqual([
-      [0, 2],
-      [11, 13],
-      [18, 20],
-    ]);
-    expect(matchResult[0].line).toBe(parsedRegexTest.testLines[0]);
+    expect(matchResult[0].substring).toBe('9a');
+    expect(matchResult[0].range).toEqual([0, 2]);
+
+    expect(matchResult[1].substring).toBe('8a');
+    expect(matchResult[1].range).toEqual([11, 13]);
+
+    expect(matchResult[2].substring).toBe('7a');
+    expect(matchResult[2].range).toEqual([18, 20]);
   });
 
-  it('should test regex correctly, if the regex does not match', () => {
-    const regex = new RegExp('[0-9]a', 'gd');
+  it('should not return match results, if the regex does not match any test lines', () => {
+    const regex = new RegExp('[0-9]a', 'g');
 
     const parsedRegexTest: ParsedRegexTest = {
       matchingRegex: regex,
       testLines: ['bbbb'],
+      startTestIndex: 0,
     };
 
     const matchResult = RegexTester.testRegex(parsedRegexTest);
-
-    expect(matchResult.length).toBe(1);
-    expect(matchResult[0].matched).toBe(false);
-    expect(matchResult[0].indexes).toEqual([]);
-    expect(matchResult[0].line).toBe(parsedRegexTest.testLines[0]);
+    expect(matchResult.length).toBe(0);
   });
 
-  it('should test regex correctly, if the regex has multine flag', () => {
-    const regex = new RegExp('[0-9]a', 'gdm');
+  it('should test regex correctly, if there are many test lines and has multine flag', () => {
+    const regex = new RegExp('^[0-9]a', 'gm');
 
     const parsedRegexTest: ParsedRegexTest = {
       matchingRegex: regex,
-      testLines: ['bbb9a\n9abbb\nbb9abb'],
+      testLines: ['9abbb', 'bb8ab', '7abb'],
+      startTestIndex: 0,
     };
 
     const matchResult = RegexTester.testRegex(parsedRegexTest);
+    expect(matchResult.length).toBe(2);
 
+    expect(matchResult[0].substring).toBe('9a');
+    expect(matchResult[0].range).toEqual([0, 2]);
+
+    expect(matchResult[1].substring).toBe('7a');
+    expect(matchResult[1].range).toEqual([12, 14]);
+  });
+
+  it('should test regex correctly, if there are many test lines and does not have multine flag', () => {
+    const regex = new RegExp('^[0-9]a', 'g');
+
+    const parsedRegexTest: ParsedRegexTest = {
+      matchingRegex: regex,
+      testLines: ['9abbb\nbb8ab\n7abb'],
+      startTestIndex: 0,
+    };
+
+    const matchResult = RegexTester.testRegex(parsedRegexTest);
     expect(matchResult.length).toBe(1);
-    expect(matchResult[0].matched).toBe(true);
-    expect(matchResult[0].indexes).toEqual([
-      [3, 5],
-      [6, 8],
-      [14, 16],
-    ]);
-    expect(matchResult[0].line).toBe(parsedRegexTest.testLines[0]);
+
+    expect(matchResult[0].substring).toBe('9a');
+    expect(matchResult[0].range).toEqual([0, 2]);
   });
 });
