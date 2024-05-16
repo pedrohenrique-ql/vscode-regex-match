fed43d90c7000c3b8dd29dc4cc6a0a805c8e0fe1
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-05-16 20:45:20 -0300

    test: capturing the matching range of regex groups

--- src/tests/unit/regex-tester.test.ts
+++ src/tests/unit/regex-tester.test.ts
@@ -24,6 +24,7 @@ describe('Regex Tester', () => {
 
     expect(matchResult[0].substring).toBe('9a');
     expect(matchResult[0].range).toEqual([3, 5]);
+    expect(matchResult[0].groupRanges).toBeUndefined();
   });
 
   it('should test regex correctly, if the regex matches and has multiple matches in one line', () => {
@@ -40,12 +41,15 @@ describe('Regex Tester', () => {
 
     expect(matchResult[0].substring).toBe('9a');
     expect(matchResult[0].range).toEqual([0, 2]);
+    expect(matchResult[0].groupRanges).toBeUndefined();
 
     expect(matchResult[1].substring).toBe('8a');
     expect(matchResult[1].range).toEqual([11, 13]);
+    expect(matchResult[1].groupRanges).toBeUndefined();
 
     expect(matchResult[2].substring).toBe('7a');
     expect(matchResult[2].range).toEqual([18, 20]);
+    expect(matchResult[2].groupRanges).toBeUndefined();
   });
 
   it('should not return match results, if the regex does not match any test lines', () => {
@@ -75,9 +79,11 @@ describe('Regex Tester', () => {
 
     expect(matchResult[0].substring).toBe('9a');
     expect(matchResult[0].range).toEqual([0, 2]);
+    expect(matchResult[0].groupRanges).toBeUndefined();
 
     expect(matchResult[1].substring).toBe('7a');
     expect(matchResult[1].range).toEqual([12, 14]);
+    expect(matchResult[1].groupRanges).toBeUndefined();
   });
 
   it('should test regex correctly, if there are many test lines and does not have multine flag', () => {
@@ -94,6 +100,7 @@ describe('Regex Tester', () => {
 
     expect(matchResult[0].substring).toBe('9a');
     expect(matchResult[0].range).toEqual([0, 2]);
+    expect(matchResult[0].groupRanges).toBeUndefined();
   });
 
   it('should test regex correctly, if the start test index is greater than zero', () => {
@@ -110,11 +117,65 @@ describe('Regex Tester', () => {
 
     expect(matchResult[0].substring).toBe('9a');
     expect(matchResult[0].range).toEqual([15, 17]);
+    expect(matchResult[0].groupRanges).toBeUndefined();
 
     expect(matchResult[1].substring).toBe('8a');
     expect(matchResult[1].range).toEqual([23, 25]);
+    expect(matchResult[1].groupRanges).toBeUndefined();
 
     expect(matchResult[2].substring).toBe('7a');
     expect(matchResult[2].range).toEqual([27, 29]);
+    expect(matchResult[2].groupRanges).toBeUndefined();
+  });
+
+  it('should capture group ranges', () => {
+    const regex = new RegExp('[0-9]ax(abc)', 'gmd');
+
+    const parsedRegexTest: ParsedRegexTest = {
+      matchingRegex: regex,
+      testLines: ['bb8axabcx'],
+      startTestIndex: 0,
+    };
+
+    const matchResult = RegexTester.testRegex(parsedRegexTest);
+    expect(matchResult.length).toBe(1);
+
+    expect(matchResult[0].substring).toBe('8axabc');
+    expect(matchResult[0].range).toEqual([2, 8]);
+
+    expect(matchResult[0].groupRanges).toBeDefined();
+    expect(matchResult[0].groupRanges!.length).toBe(1);
+    expect(matchResult[0].groupRanges).toEqual([[5, 8]]);
+  });
+
+  it('should capture group ranges of each line, if there are many test lines', () => {
+    const regex = new RegExp('[0-9]ax(abc)', 'gmd');
+
+    const parsedRegexTest: ParsedRegexTest = {
+      matchingRegex: regex,
+      testLines: ['bb8axabcx', '9axabc123', '1234axabc'],
+      startTestIndex: 0,
+    };
+
+    const matchResult = RegexTester.testRegex(parsedRegexTest);
+    expect(matchResult.length).toBe(3);
+
+    expect(matchResult[0].substring).toBe('8axabc');
+    expect(matchResult[0].range).toEqual([2, 8]);
+    expect(matchResult[0].groupRanges).toBeDefined();
+    expect(matchResult[0].groupRanges!.length).toBe(1);
+    expect(matchResult[0].groupRanges).toEqual([[5, 8]]);
+
+    expect(matchResult[1].substring).toBe('9axabc');
+    expect(matchResult[1].range).toEqual([10, 16]);
+    expect(matchResult[1].groupRanges).toBeDefined();
+    expect(matchResult[1].groupRanges!.length).toBe(1);
+    expect(matchResult[1].groupRanges).toEqual([[13, 16]]);
+
+    expect(matchResult[2].substring).toBe('4axabc');
+    expect(matchResult[2].range).toEqual([23, 29]);
+    expect(matchResult[2].groupRanges).toBeDefined();
+    expect(matchResult[2].groupRanges!.length).toBe(1);
+    expect(matchResult[2].groupRanges).toEqual([[26, 29]]);
   });
 });
