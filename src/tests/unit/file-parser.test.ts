ba31a060ca3abe0d98aaa5617830d51f21ef10d2
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-06-09 12:38:11 -0300

    test: format and syntax errors in file parsing

--- src/tests/unit/file-parser.test.ts
+++ src/tests/unit/file-parser.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from 'vitest';
 
+import RegexMatchFormatError, { DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE } from '@/exceptions/RegexMatchFormatError';
+import RegexSyntaxError from '@/exceptions/RegexSyntaxError';
+
 import FileParser from '../../FileParser';
 
 describe('File Parser', () => {
@@ -10,10 +13,10 @@ describe('File Parser', () => {
     expect(regexTests).not.toBeNull();
     expect(regexTests).toHaveLength(1);
 
-    expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]a/dg);
-    expect(regexTests![0].getTestLines()).toHaveLength(1);
-    expect(regexTests![0].getTestLines()[0]).toBe('bb9abb');
-    expect(regexTests![0].getStartTestIndex()).toBe(14);
+    expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]a/dg);
+    expect(regexTests[0].getTestLines()).toHaveLength(1);
+    expect(regexTests[0].getTestLines()[0]).toBe('bb9abb');
+    expect(regexTests[0].getStartTestIndex()).toBe(14);
   });
 
   it('should parse file content correctly, if the regex has multiline flag', () => {
@@ -23,11 +26,11 @@ describe('File Parser', () => {
     expect(regexTests).not.toBeNull();
     expect(regexTests).toHaveLength(1);
 
-    expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
-    expect(regexTests![0].getTestLines()).toHaveLength(2);
-    expect(regexTests![0].getTestLines()[0]).toBe('bb9abb');
-    expect(regexTests![0].getTestLines()[1]).toBe('2a');
-    expect(regexTests![0].getStartTestIndex()).toBe(15);
+    expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
+    expect(regexTests[0].getTestLines()).toHaveLength(2);
+    expect(regexTests[0].getTestLines()[0]).toBe('bb9abb');
+    expect(regexTests[0].getTestLines()[1]).toBe('2a');
+    expect(regexTests[0].getStartTestIndex()).toBe(15);
   });
 
   it('should parse file content correctly, if the regex does not have multiline flag', () => {
@@ -37,10 +40,10 @@ describe('File Parser', () => {
     expect(regexTests).not.toBeNull();
     expect(regexTests).toHaveLength(1);
 
-    expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]a/dg);
-    expect(regexTests![0].getTestLines()).toHaveLength(1);
-    expect(regexTests![0].getTestLines()[0]).toBe('bb9abb\n2a');
-    expect(regexTests![0].getStartTestIndex()).toBe(14);
+    expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]a/dg);
+    expect(regexTests[0].getTestLines()).toHaveLength(1);
+    expect(regexTests[0].getTestLines()[0]).toBe('bb9abb\n2a');
+    expect(regexTests[0].getStartTestIndex()).toBe(14);
   });
 
   it('should set the default flags in matching regex, if the flags are not provided', () => {
@@ -50,10 +53,10 @@ describe('File Parser', () => {
     expect(regexTests).not.toBeNull();
     expect(regexTests).toHaveLength(1);
 
-    expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
-    expect(regexTests![0].getTestLines()).toHaveLength(1);
-    expect(regexTests![0].getTestLines()[0]).toBe('bb9abb');
-    expect(regexTests![0].getStartTestIndex()).toBe(13);
+    expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
+    expect(regexTests[0].getTestLines()).toHaveLength(1);
+    expect(regexTests[0].getTestLines()[0]).toBe('bb9abb');
+    expect(regexTests[0].getStartTestIndex()).toBe(13);
   });
 
   it("should set the required 'd' flag in matching regex", () => {
@@ -63,53 +66,116 @@ describe('File Parser', () => {
     expect(regexTests).not.toBeNull();
     expect(regexTests).toHaveLength(1);
 
-    expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
-    expect(regexTests![0].getTestLines()).toHaveLength(1);
-    expect(regexTests![0].getTestLines()[0]).toBe('bb9abb');
-    expect(regexTests![0].getStartTestIndex()).toBe(15);
+    expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]a/dgm);
+    expect(regexTests[0].getTestLines()).toHaveLength(1);
+    expect(regexTests[0].getTestLines()[0]).toBe('bb9abb');
+    expect(regexTests[0].getStartTestIndex()).toBe(15);
   });
 
   it('should throw an error if the matching regex is invalid', () => {
     const fileContent = '/(?/gm\n---\nbb9abb\n---';
 
-    expect(() => FileParser.parseFileContent(fileContent)).toThrowError();
+    try {
+      FileParser.parseFileContent(fileContent);
+      expect.unreachable('Expected to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(RegexSyntaxError);
+
+      const regexMatchFormatError = error as RegexSyntaxError;
+      expect(regexMatchFormatError.message).toContain('Invalid regular expression');
+      expect(regexMatchFormatError.line).toBe(0);
+    }
   });
 
-  it('should return null if the file content does not contain the test area delimiter', () => {
+  it('should throw error if the file content does not contain the test area delimiter', () => {
     const fileContent = '/[0-9]a/g\nbb9abb\n---';
 
-    const regexTests = FileParser.parseFileContent(fileContent);
-    expect(regexTests).toBeNull();
+    try {
+      FileParser.parseFileContent(fileContent);
+      expect.unreachable('Expected to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(RegexMatchFormatError);
+
+      const regexMatchFormatError = error as RegexMatchFormatError;
+      expect(regexMatchFormatError.message).toBe(DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE);
+      expect(regexMatchFormatError.line).toBe(0);
+    }
   });
 
-  it('should return null if the file content does not contain the matching regex', () => {
+  it('should throw error if the file content does not contain the matching regex', () => {
     const fileContent = '---\nbb9abb\n---';
 
-    const regexTests = FileParser.parseFileContent(fileContent);
-    expect(regexTests).toBeNull();
+    try {
+      FileParser.parseFileContent(fileContent);
+      expect.unreachable('Expected to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(RegexMatchFormatError);
+
+      const regexMatchFormatError = error as RegexMatchFormatError;
+      expect(regexMatchFormatError.message).toBe(DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE);
+      expect(regexMatchFormatError.line).toBe(0);
+    }
   });
 
-  it('should return null if the file content does not contain the test area', () => {
+  it('should throw error if the file content does not contain the test area', () => {
     const fileContent = '/[0-9]a/g\n---';
 
-    const regexTests = FileParser.parseFileContent(fileContent);
-    expect(regexTests).toBeNull();
+    try {
+      FileParser.parseFileContent(fileContent);
+      expect.unreachable('Expected to throw an error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(RegexMatchFormatError);
+
+      const regexMatchFormatError = error as RegexMatchFormatError;
+      expect(regexMatchFormatError.message).toBe(DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE);
+      expect(regexMatchFormatError.line).toBe(0);
+    }
   });
 
   describe('Multiple Regex Tests', () => {
-    it('should return null if the second regex test does not have opening delimiter', () => {
+    it('should throw error if the second regex test does not have opening delimiter', () => {
       const fileContent = '/[0-9]/\n---\ntest\ntest\n---\n/[0-9]/';
 
-      const regexTests = FileParser.parseFileContent(fileContent);
-      expect(regexTests).toBeNull();
+      try {
+        FileParser.parseFileContent(fileContent);
+        expect.unreachable('Expected to throw an error');
+      } catch (error) {
+        expect(error).toBeInstanceOf(RegexMatchFormatError);
+
+        const regexMatchFormatError = error as RegexMatchFormatError;
+        expect(regexMatchFormatError.message).toBe(DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE);
+        expect(regexMatchFormatError.line).toBe(0);
+      }
     });
 
-    it('should return null if the second regex test does not have closing delimiter', () => {
+    it('should throw error if the second regex test does not have closing delimiter', () => {
       const fileContent = '/[0-9]/\n---\ntest\ntest\n---\n/[0-9]/\n---\ntest\ntest';
 
-      const regexTests = FileParser.parseFileContent(fileContent);
-      console.log(regexTests);
-      expect(regexTests).toBeNull();
+      try {
+        FileParser.parseFileContent(fileContent);
+        expect.unreachable('Expected to throw an error');
+      } catch (error) {
+        expect(error).toBeInstanceOf(RegexMatchFormatError);
+
+        const regexMatchFormatError = error as RegexMatchFormatError;
+        expect(regexMatchFormatError.message).toBe(DEFAULT_REGEX_MATCH_FORMAT_ERROR_MESSAGE);
+        expect(regexMatchFormatError.line).toBe(0);
+      }
+    });
+
+    it('should throw an error if the second matching regex is invalid', () => {
+      const fileContent = '/[0-9]/gm\n---\nbb9abb\n---\n/[0-9](/gm\n---\n9\n---';
+
+      try {
+        FileParser.parseFileContent(fileContent);
+        expect.unreachable('Expected to throw an error');
+      } catch (error) {
+        expect(error).toBeInstanceOf(RegexSyntaxError);
+
+        const regexMatchFormatError = error as RegexSyntaxError;
+        expect(regexMatchFormatError.message).toContain('Invalid regular expression');
+        expect(regexMatchFormatError.line).toBe(4);
+      }
     });
 
     it('should parse multiple regex tests correctly', () => {
@@ -119,17 +185,17 @@ describe('File Parser', () => {
       expect(regexTests).not.toBeNull();
       expect(regexTests).toHaveLength(2);
 
-      expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
-      expect(regexTests![0].getTestLines()).toHaveLength(2);
-      expect(regexTests![0].getTestLines()[0]).toBe('test1');
-      expect(regexTests![0].getTestLines()[1]).toBe('test2');
-      expect(regexTests![0].getStartTestIndex()).toBe(14);
+      expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
+      expect(regexTests[0].getTestLines()).toHaveLength(2);
+      expect(regexTests[0].getTestLines()[0]).toBe('test1');
+      expect(regexTests[0].getTestLines()[1]).toBe('test2');
+      expect(regexTests[0].getStartTestIndex()).toBe(14);
 
-      expect(regexTests![1].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
-      expect(regexTests![1].getTestLines()).toHaveLength(2);
-      expect(regexTests![1].getTestLines()[0]).toBe('test3');
-      expect(regexTests![1].getTestLines()[1]).toBe('test4');
-      expect(regexTests![1].getStartTestIndex()).toBe(44);
+      expect(regexTests[1].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
+      expect(regexTests[1].getTestLines()).toHaveLength(2);
+      expect(regexTests[1].getTestLines()[0]).toBe('test3');
+      expect(regexTests[1].getTestLines()[1]).toBe('test4');
+      expect(regexTests[1].getStartTestIndex()).toBe(44);
     });
 
     it('should parse multiple regex tests correctly, if the second regex test does not have multiline flag', () => {
@@ -139,16 +205,16 @@ describe('File Parser', () => {
       expect(regexTests).not.toBeNull();
       expect(regexTests).toHaveLength(2);
 
-      expect(regexTests![0].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
-      expect(regexTests![0].getTestLines()).toHaveLength(2);
-      expect(regexTests![0].getTestLines()[0]).toBe('test1');
-      expect(regexTests![0].getTestLines()[1]).toBe('test2');
-      expect(regexTests![0].getStartTestIndex()).toBe(14);
+      expect(regexTests[0].getMatchingRegex()).toStrictEqual(/[0-9]/dgm);
+      expect(regexTests[0].getTestLines()).toHaveLength(2);
+      expect(regexTests[0].getTestLines()[0]).toBe('test1');
+      expect(regexTests[0].getTestLines()[1]).toBe('test2');
+      expect(regexTests[0].getStartTestIndex()).toBe(14);
 
-      expect(regexTests![1].getMatchingRegex()).toStrictEqual(/[0-9]/di);
-      expect(regexTests![1].getTestLines()).toHaveLength(1);
-      expect(regexTests![1].getTestLines()[0]).toBe('test3\ntest4');
-      expect(regexTests![1].getStartTestIndex()).toBe(43);
+      expect(regexTests[1].getMatchingRegex()).toStrictEqual(/[0-9]/di);
+      expect(regexTests[1].getTestLines()).toHaveLength(1);
+      expect(regexTests[1].getTestLines()[0]).toBe('test3\ntest4');
+      expect(regexTests[1].getStartTestIndex()).toBe(43);
     });
   });
 });
