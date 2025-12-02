607c827646897cb80c5beabf47fc71b4ee4b6071
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2025-12-01 21:57:07 -0300

    chore: update glob and mocha versions

--- src/tests/integration/suite/index.ts
+++ src/tests/integration/suite/index.ts
@@ -5,31 +5,33 @@ import * as path from 'path';
 export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: 'tdd',
+    color: true,
+    timeout: 10000,
   });
 
   const testsRoot = path.resolve(__dirname, '..');
 
-  return new Promise((c, e) => {
-    void glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
-      if (err) {
-        e(err);
-        return;
-      }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const files = await glob('**/**.test.js', { cwd: testsRoot });
 
       files.forEach((file: string) => mocha.addFile(path.resolve(testsRoot, file)));
 
       try {
         mocha.run((failures) => {
           if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
+            reject(new Error(`${failures} tests failed.`));
           } else {
-            c();
+            resolve();
           }
         });
-      } catch (_error) {
-        console.error(err);
-        e(err);
+      } catch (error) {
+        console.error('Failed to run tests:', error);
+        reject(error);
       }
-    });
+    } catch (error) {
+      console.error('Failed to find test files:', error);
+      reject(error);
+    }
   });
 }
