06fb47212e0e2da3ca52de7a89785f997c84782a
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2026-01-14 21:11:17 -0300

    test: apply regex to code CodeLens

--- src/tests/mocks/vscode.ts
+++ src/tests/mocks/vscode.ts
@@ -1,12 +1,76 @@
 export const Uri = {
   file: (path: string) => ({ fsPath: path, toString: () => `file://${path}` }),
+  parse: (uri: string) => ({ fsPath: uri, toString: () => uri }),
 };
+
+export class Position {
+  constructor(
+    public line: number,
+    public character: number,
+  ) {}
+}
 
 export class Range {
   constructor(
-    public startLine: number,
-    public startChar: number,
-    public endLine: number,
-    public endChar: number,
+    public start: Position,
+    public end: Position,
   ) {}
 }
+
+export class EventEmitter<T> {
+  private listeners: ((e: T) => void)[] = [];
+
+  get event() {
+    return (listener: (e: T) => void) => {
+      this.listeners.push(listener);
+      return {
+        dispose: () => {
+          const index = this.listeners.indexOf(listener);
+          if (index >= 0) {
+            this.listeners.splice(index, 1);
+          }
+        },
+      };
+    };
+  }
+
+  fire(data: T): void {
+    this.listeners.forEach((listener) => listener(data));
+  }
+}
+
+export class CodeLens {
+  constructor(
+    public range: Range,
+    public command?: {
+      title: string;
+      command: string;
+      arguments?: unknown[];
+    },
+  ) {}
+}
+
+export const workspace = {
+  textDocuments: [],
+  getConfiguration: () => ({
+    get: () => undefined,
+    has: () => false,
+    inspect: () => undefined,
+    update: () => Promise.resolve(),
+  }),
+  onDidOpenTextDocument: () => ({
+    dispose: () => {
+      /* empty */
+    },
+  }),
+  onDidCloseTextDocument: () => ({
+    dispose: () => {
+      /* empty */
+    },
+  }),
+  onDidChangeTextDocument: () => ({
+    dispose: () => {
+      /* empty */
+    },
+  }),
+};
